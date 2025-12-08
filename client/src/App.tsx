95f6b81472a8a156bcbde535dc1e2e5600a21e73
purpleskies <49821513-purpleskies@users.noreply.replit.com>
2025-12-08 08:13:57 +0000

    Restored to '1a325d820122efeba573feeb4b3b7a533b46ea33'
    Replit-Restored-To: 1a325d820122efeba573feeb4b3b7a533b46ea33

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,7 +3,6 @@ import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
-import { SharedLayout } from "@/components/shared-layout";
 import NotFound from "@/pages/not-found";
 import Home from "@/pages/home";
 
@@ -21,9 +20,7 @@ function App() {
     <QueryClientProvider client={queryClient}>
       <TooltipProvider>
         <Toaster />
-        <SharedLayout>
-          <Router />
-        </SharedLayout>
+        <Router />
       </TooltipProvider>
     </QueryClientProvider>
   );
